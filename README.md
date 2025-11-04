968b6b94bb9b5d4d79b8c9f96779edaea488ea45
pavlovadb <pavlovadb@gmail.com>
2025-11-04 10:00:34 -0800

    adds second experiment partition_pdf function

--- README.md
+++ README.md
@@ -6,5 +6,8 @@ To get started, just:
 docker compose up
 ```
 
-It should partition the `./files/climbing-report.pdf` and write the results to 
-`./files/output.txt`.
+1. It should partition the `./files/wwf_tigers_e_1.pdf` and write the results to 
+`./files/auto-partition-output.txt`. This is fast.
+
+1. It should partition the `./files/wwf_tigers_e_1.pdf` again using the `partition_pdf` function using `hi-res` and write the results to 
+`./files/pdf-partition-output.txt`. This takes a lot more time.
--- src/main.py
+++ src/main.py
@@ -1,6 +1,33 @@
 from unstructured.partition.auto import partition
+from unstructured.partition.pdf import partition_pdf
 
-
-elements = partition(filename="../files/climbing-report.pdf")
-with open("../files/output.txt", "w") as f:
+# !!!!!!! EXPERIMENT 1:
+elements = partition(filename="../files/wwf_tigers_e_1.pdf")
+with open("../files/auto-partition-output.txt", "w") as f:
     f.write("\n\n".join([str(el) for el in elements]))
+
+# !!!!!! EXPERIMENT 2:
+import os
+os.makedirs("../files/images/", exist_ok=True)
+
+# I am using extract_image_block_to_payload=False (instead I write write the images
+# to the images folder)
+elementsTwo = partition_pdf(filename="../files/wwf_tigers_e_1.pdf",
+                            strategy="hi_res", extract_images_in_pdf=True,
+                            extract_image_block_types=["Image", "Table"],
+                            extract_image_block_to_payload=False,
+                            extract_image_block_output_dir="../files/images/"
+                            )
+with open("../files/pdf-partition-output.txt", "w") as f:
+    f.write("\n\n".join([str(el) for el in elements]))
+
+# Write text content to file
+with open("../files/pdf-partition-output.txt", "w") as f:
+    f.write("\n\n".join([str(el) for el in elementsTwo]))
+
+# Check how many images were saved to filesystem
+import glob
+saved_images = glob.glob("../files/images/*")
+print(f"Total images saved to filesystem: {len(saved_images)}")
+for img in saved_images:
+    print(f"  {img}")
